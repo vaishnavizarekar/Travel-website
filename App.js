f3b34a878a2fdae01c9413b77a2ca058f5e511b7
vaishnavizarekar <147190304+vaishnavizarekar@users.noreply.github.com>
2024-03-19 23:23:09 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import HeroSection from './HeroSection';
+import DestinationsSection from './DestinationsSection';
+import ContactForm from './ContactForm';
+import Footer from './Footer';
+
+const App = () => (
+  <div>
+    <Header />
+    <HeroSection />
+    <DestinationsSection />
+    <ContactForm />
+    <Footer />
+  </div>
+);
+
+export default App;
