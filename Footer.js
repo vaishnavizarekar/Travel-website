1190057b6392e8f437e97452592625e39d61a9d1
vaishnavizarekar <147190304+vaishnavizarekar@users.noreply.github.com>
2024-03-19 23:22:30 +0530

    Footer.js

---
+++ Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <div className="footer-content">
+      <p>&copy; 2024 Travel Explorer. All Rights Reserved.</p>
+      <div className="social-media">
+        <a href="https://www.facebook.com"><img src="facebook-icon.png" alt="Facebook" /></a>
+        <a href="https://www.twitter.com"><img src="twitter-icon.png" alt="Twitter" /></a>
+        <a href="https://www.instagram.com"><img src="instagram-icon.png" alt="Instagram" /></a>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
